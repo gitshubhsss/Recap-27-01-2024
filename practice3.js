451020bbbf33d2ecbde3cf71e93b65ae412dbb1e
gitshubhsss <shubhamranjane16@gmail.com>
2024-01-27 20:46:07 +0530

    add the new button in the body tag

---
+++ practice3.js
@@ -0,0 +1,6 @@
+let newBtn=document.createElement("button");
+newBtn.innerText="click me";
+newBtn.style.backgroundColor="red";
+newBtn.style.color="white";
+
+document.querySelector("body").append(newBtn)
