7828c1cee9da3664cd659b78b3a525ce2ebc84b7
gitshubhsss <shubhamranjane16@gmail.com>
2024-01-27 18:53:36 +0530

    accecing and changing elements using dom

---
+++ practice1.js
@@ -0,0 +1,6 @@
+// Create a h2 heading element with text -"Hellow JavaScript".Append "from Apana Collage students"to this using js
+let str=document.querySelector("#heading");
+console.log(str.innerText);
+
+str.innerText=str.innerText+" from apana collage";
+
