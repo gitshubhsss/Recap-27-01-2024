e00139e6d611008d61f645c2f7ed189ee8c9e5f5
gitshubhsss <shubhamranjane16@gmail.com>
2024-01-27 20:39:08 +0530

    add the elements add the top of the body

--- createElements.js
+++ createElements.js
@@ -1,3 +1,9 @@
+let newHeading=document.createElement("h1");
+newHeading.innerText="this is above the body tag";
+
+document.querySelector("body").prepend(newHeading)
+
 let newBtn=document.createElement("button");
 newBtn.innerText="click me"
 document.getElementById("box").append(newBtn);
+
